361091121d16347c5781fc70d12eda2e9990d358
Roberto Livi <robertomlivi@outlook.com>
2020-01-27 16:18:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,16 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(hash)
   if hash.empty?
     nil
   end
-  a = hash.keys[0]
-  b = hash.values[0]
+  i = hash.values[0] + 1
+  key = ""
   hash.each do |k, v|
-      if b < v
-        k = key
+      if v < i
+      i = v
+      key = k
+      end
   end
-  key
+  puts key
 end
 
 tennis_racquets = {
