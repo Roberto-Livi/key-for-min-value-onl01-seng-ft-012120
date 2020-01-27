9cc4f00744770e7caa2b03543644e82e98bad19d
Roberto Livi <robertomlivi@outlook.com>
2020-01-27 15:59:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,31 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  if hash.empty?
+    nil
+  end
+  a = hash.keys[0]
+  b = hash.values[0]
+  hash.each do |k, v|
+      if b < v
+        k = key
+  end
+  key
 end
+
+tennis_racquets = {
+  :Yonex => 140,
+  :Babolat => 250,
+  :Prince => 100,
+  :Wilson => 320
+}
+key_for_min_value(tennis_racquets)
+
+fruits = {
+  :Acai => 20,
+  :Strawberries => 120,
+  :Bananas => 300,
+  :Apples => 280
+}
+key_for_min_value(fruits)
